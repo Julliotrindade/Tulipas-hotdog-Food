a9d9b64b70c1a5106045a6f25c01e209b088bd84
Julliotrindade <julliotrindade01@gmail.com>
2026-05-14 16:44:15 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function PedidoHotdog() {
   const [produtos, setProdutos] = useState([
     {
       nome: "Hotdog Tradicional",
-      preco: 7,
+      preco: 18,
       qtd: 0,
       max: 10,
       img: "https://img.cdndsgni.com/preview/10531382.jpg",
