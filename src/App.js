273804e29a590c087467753d8e0370b72d22901f
Julliotrindade <julliotrindade01@gmail.com>
2026-05-13 22:22:05 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function PedidoHotdog() {
+import App from "./App"; {
   const [produtos, setProdutos] = useState([
     {
       nome: "Hotdog Simples",
